8c94279c698a0bc274d7e5d87bb7838b8642f1c1
Janley Crsiologo <crisoja@oocl.com>
2021-08-09 19:27:30 +0800

    [WordFrequency][Janley] refactor: rename w to word in parallelStream

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -7,28 +7,20 @@ public class WordFrequencyGame {
     private static final String ADD_SPACE_AND_ONE = " 1";
     private Map<String, Integer> wordsCounts;
 
-    public String getResult(String sentence){
-
-
-            try {
-
-                List<WordInfo> wordInfoList = calculateWordFrequency(sentence);
-
-                sortWords(wordInfoList);
-
-
-                return getWordWithCount(wordInfoList).toString();
-            } catch (Exception e) {
-
-
-                return "Calculate Error";
-            }
+    public String getResult(String sentence) {
+        try {
+            List<WordInfo> wordInfoList = calculateWordFrequency(sentence);
+            sortWords(wordInfoList);
+            return getWordWithCount(wordInfoList).toString();
+        } catch (Exception e) {
+            return "Calculate Error";
         }
+    }
 
     private StringJoiner getWordWithCount(List<WordInfo> wordInfoList) {
         StringJoiner allWordsWithCount = new StringJoiner("\n");
         for (WordInfo word : wordInfoList) {
-            String wordWithCount = word.getValue() + " " +word.getWordCount();
+            String wordWithCount = word.getValue() + " " + word.getWordCount();
             allWordsWithCount.add(wordWithCount);
         }
         return allWordsWithCount;
@@ -38,18 +30,14 @@ public class WordFrequencyGame {
         wordInfoList.sort((word1, word2) -> word2.getWordCount() - word1.getWordCount());
     }
 
-
     private List<WordInfo> calculateWordFrequency(String sentence) {
         List<String> words = Arrays.asList(sentence.split(BLANK_SPACE));
-
         wordsCounts = words.parallelStream().
                 collect(Collectors.toConcurrentMap(
-                        w -> w, w -> 1, Integer::sum));
-
-            return wordsCounts.entrySet()
-                         .stream()
-                         .map(input -> new WordInfo(input.getKey(),input.getValue()))
-                         .collect(Collectors.toList());
-        }
-
+                        word -> word, word -> 1, Integer::sum));
+        return wordsCounts.entrySet()
+                .stream()
+                .map(input -> new WordInfo(input.getKey(), input.getValue()))
+                .collect(Collectors.toList());
+    }
 }
