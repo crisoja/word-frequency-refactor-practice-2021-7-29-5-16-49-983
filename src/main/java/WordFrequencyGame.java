350d214fcc90cefc317d07fa953eb1a115996892
Janley Crsiologo <crisoja@oocl.com>
2021-08-09 16:56:47 +0800

    [WordFrequencyGame][Janley] refactor: simplify the method calculateWordFrequency and used hash map

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,9 +1,11 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
 
-    public static final String BLANK_SPACE = "\\s+";
-    public static final String ADD_SPACE_AND_ONE = " 1";
+    private static final String BLANK_SPACE = "\\s+";
+    private static final String ADD_SPACE_AND_ONE = " 1";
+    private Map<String, Integer> wordsCounts;
 
     public String getResult(String sentence){
 
@@ -33,47 +35,21 @@ public class WordFrequencyGame {
         }
     }
 
+
+
     private List<Input> calculateWordFrequency(String sentence) {
-        //split the input string with 1 to n pieces of spaces
-        String[] arr = sentence.split(BLANK_SPACE);
+        List<String> words = Arrays.asList(sentence.split(BLANK_SPACE));
 
-        List<Input> inputList = new ArrayList<>();
-        for (String s : arr) {
-            Input input = new Input(s, 1);
-            inputList.add(input);
+        wordsCounts = words.parallelStream().
+                collect(Collectors.toConcurrentMap(
+                        w -> w, w -> 1, Integer::sum));
+
+            return wordsCounts.entrySet()
+                         .stream()
+                         .map(input -> new Input(input.getKey(),input.getValue()))
+                         .collect(Collectors.toList());
         }
 
-        //get the map for the next step of sizing the same word
-        Map<String, List<Input>> map =getListMap(inputList);
-
-        List<Input> list = new ArrayList<>();
-        for (Map.Entry<String, List<Input>> entry : map.entrySet()){
-            Input input = new Input(entry.getKey(), entry.getValue().size());
-            list.add(input);
-        }
-        inputList = list;
-        return inputList;
-    }
-
-
-    private Map<String,List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
-        for (Input input :  inputList){
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())){
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getValue(), arr);
-            }
-
-            else {
-                map.get(input.getValue()).add(input);
-            }
-        }
-
-
-        return map;
-    }
 
 
 }
